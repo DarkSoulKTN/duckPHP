620d1aec6d2b4fc93e4a9250904c4c9d2fd37309
Quackster <imploderific@gmail.com>
2012-07-09 17:01:57 +1000

    No longer need these files

--- load.php
+++ load.php
@@ -1,23 +1,3 @@
-<?php
-
-include "global.php";
-
-/*
- * Load index page
- */
-$tpl->load("index");
-
-/*
- * Set some misc variables
- */
-$tpl->set("email", "quackphp@quackster.net");
-
-/*
- * Replace plugin tag with the chosen class name
- */
-
-/*
- * Output the contents
- */
-$tpl->display();
-?>
+/********************************
+    OLD FILE. PLEASE DELETE
+*********************************/
--- test.php
+++ test.php
@@ -1,25 +1,3 @@
-<?php
-
-include "global.php";
-
-/*
- * Load index page
- */
-$tpl->load("index");
-
-/*
- * Set some misc variables
- */
-$tpl->set("email", "quackphp@quackster.net");
-
-/*
- * Replace plugin tag with the chosen class name
- */
-$tpl->plugin("footer");
-
-/*
- * Output the contents
- */
-$tpl->display();
-
-?>
+/********************************
+    OLD FILE. PLEASE DELETE
+*********************************/
